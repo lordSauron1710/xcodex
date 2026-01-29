a9e5e058b89cde81d240cc3e613cbd32e80187b2
Sandeep Vangara <vangara.sandeep17@gmail.com>
2026-01-29 10:06:06 -0500

    Initial Commit

---
+++ xcodex/xcodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodexApp.swift
+//  xcodex
+//
+//  Created by Sandeep Vangara on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct xcodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
